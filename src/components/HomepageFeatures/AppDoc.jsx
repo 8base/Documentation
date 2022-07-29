6de6b0d9497fd2e75074cf682e6bdc65254e13f3
Sebastian Scholl <sebscholl@Sebastians-MacBook-Pro.local>
2022-07-29 15:31:34 -0600

    home page lists for sections with links

--- src/components/HomepageFeatures/AppDoc.jsx
+++ src/components/HomepageFeatures/AppDoc.jsx
@@ -1,64 +1,49 @@
 import React from "react";
 
-import { frontendDocsSidebar } from "../../../sidebars/_frontend";
-import { backendDocsSidebar } from "../../../sidebars/_backend";
-
 import styles from "./styles.module.css";
 import Link from "@docusaurus/Link";
 
-function countWord(word) {
-  console.log("word=> ", word, " count=> ", word.trim().split(/\s+/).length);
-  return word.trim().split(/\s+/).length;
+const backendList = {
+  "Welcome": "https://docs.8base.com/backend",
+  "Getting Started": "https://docs.8base.com/backend/getting-started",
+  "Backend Console": "https://docs.8base.com/backend/overview",
+  "GraphQL API": "https://docs.8base.com/backend/graphql-api",
+  "Custom Functions": "https://docs.8base.com/backend/custom-functions",
+  "Plugins and Integrations": "https://docs.8base.com/backend/plugins-integrations",
+  "Local Environment": "https://docs.8base.com/backend/development-tools/dev-env",
+  "Command-line (CLI)": "https://docs.8base.com/backend/development-tools/cli",
 }
 
-function DocumentList({ data, URL }) {
-  console.log(URL)
+const frontendList = {
+  "Getting Started": "https://docs.8base.com/frontend/building-frontends-app-builder-walkthrough/introduction",
+  "App Editor": "https://docs.8base.com/frontend/app-editor/introduction",
+  "App Styling": "https://docs.8base.com/frontend/custom-app-styling/introduction",
+  "Responsive Design": "https://docs.8base.com/frontend/responsive-design-in-app-builder/introduction",
+  "Application Layouts": "https://docs.8base.com/frontend/application-layouts/introduction",
+  "App Navigation": "https://docs.8base.com/frontend/app-navigation/introduction",
+  "App Components": "https://docs.8base.com/frontend/app-components/introduction",
+  "Connecting to APIs": "https://docs.8base.com/frontend/connecting-to-data-sources/introduction"
+}
+
+function DocumentList({ data }) {
   return (
     <div className={styles.doc_urls}>
       <ul className={styles.ul_home}>
-        {data.map((item) => {
-          console.log("item=>", item.items[0]);
+        {Object.entries(data).map((entry, index) => {
+          let [label, url] = entry
           return (
-            <>
-              {countWord(item.label) >= 5 ? (
-                <li className="li-breakword">
-                  <Link href={`${URL}`} style={{ color: "#0874f9" }}>
-                    {item.label}
-                  </Link>
-                </li>
-              ) : (
-                <li>
-                  <Link href={`${URL}`} style={{ color: "#0874f9" }}>
-                    {item.label}
-                  </Link>
-                </li>
-              )}
-            </>
+            <li key={'li-' + index}>
+              <Link href={`${url}`} style={{ color: "#0874f9" }}>
+                {label}
+              </Link>
+            </li>
           );
         })}
       </ul>
     </div>
   );
 }
-const AppDoc = ({ renderItem }) => {
-  console.log(
-    "side bar=>",
-    backendDocsSidebar,
-    "front-end=>",
-    frontendDocsSidebar
-  );
-  return (
-    <>
-      {renderItem == "backend" ? (
-        <DocumentList data={backendDocsSidebar} URL="/backend" />
-      ) : (
-        <DocumentList
-          data={frontendDocsSidebar}
-          URL="/frontend/getting-started/introduction"
-        />
-      )}
-    </>
-  );
-};
+
+const AppDoc = ({ renderItem }) => <DocumentList data={(renderItem == "backend" ? backendList : frontendList)} />;
 
 export default AppDoc;
