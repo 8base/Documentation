e8b22d1c138b58ea48b27d6f2335793f8e299f7f
Onat <53895969+onattech@users.noreply.github.com>
2023-10-24 23:09:32 +0300

    Updated logo
    Logo along with 'docs' text now acts as one.

--- src/theme/Navbar/Logo/index.js
+++ src/theme/Navbar/Logo/index.js
@@ -1,28 +1,35 @@
 import React from 'react';
 import Logo from '@theme-original/Navbar/Logo';
+import Link from '@docusaurus/Link';
 
 export default function LogoWrapper(props) {
   return (
     <>
       <Logo {...props} />
-      <span
-        style={{
-          marginLeft: 2,
-          fontSize: 40,
-          lineHeight: 0,
-          marginBottom: 6,
-          fontWeight: 100,
-        }}
+      <Link
+        to="/"
+        style={{ height: 40, color: '#fff', textDecoration: 'none' }}
       >
-        |
-      </span>
-      <img
-        src="/img/docs.png"
-        alt="docs logo"
-        height={23}
-        width={65}
-        style={{ marginRight: 92 }}
-      />
+        <span
+          style={{
+            marginLeft: 2,
+            fontSize: 40,
+            lineHeight: 0.8,
+            fontWeight: 100,
+          }}
+        >
+          |
+        </span>
+      </Link>
+      <Link to="/" style={{ height: '35%' }}>
+        <img
+          src="/img/docs.png"
+          alt="docs logo"
+          height={23}
+          width={65}
+          style={{ marginRight: 92 }}
+        />
+      </Link>
     </>
   );
 }
